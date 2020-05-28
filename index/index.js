1b7460fc2855814254c4818af18805870bffee9e
JellyfishMIX <2273516339@qq.com>
2020-05-28 17:23:47 +0800

    :construction: Initially completed the index page

---
+++ index/index.js
@@ -0,0 +1,21 @@
+// table-area
+let tableArray = [
+    ['23101', '12', '喜羊羊', 'A'],
+    ['11301', '07', '美羊羊', 'B']
+];
+
+const tableArea1801Class = document.getElementById('tableArea1801Class');
+
+function tableAreaAddRow(baseTable) {
+    const iLen = tableArray.length;
+    for (let i = 0; i < iLen; i++) {
+        let row = baseTable.insertRow();
+        let jArray = tableArray[i];
+        const jLen = jArray.length;
+        for (let j = 0; j < jLen; j++) {
+            let cell = row.insertCell();
+            cell.innerHTML = jArray[j];
+        }
+    }
+    tableArea1801Class.setAttribute('rowspan', tableArea1801Class.getAttribute('rowspan') + 2);
+}
